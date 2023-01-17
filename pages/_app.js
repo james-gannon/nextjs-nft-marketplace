aab18c242c56af5b26d82c3cb230d66f7bb86b02
james-gannon <mailforjamesg@gmail.com>
2023-01-17 10:31:31 -0500

    Funtional Header component and navbar update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Header from "@/components/Header"
+import Head from "next/head"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider initializeOnMount={false}>
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
 }
